5cf8fe67cba5668ff85c1ed4d316eaf77f6f6c71
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-04-07 12:35:17 -0300

    fix: Fixing SSR rendering, now it should work

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,7 +2,7 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import { route as routeFn } from 'ziggy-js';
 import { initializeTheme } from './hooks/use-appearance';
 
@@ -16,9 +16,7 @@ createInertiaApp({
     title: (title) => `SAJIC 2025 | ${title}`,
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(<App {...props} />);
+        hydrateRoot(el, <App {...props} />);
     },
     progress: {
         color: '#4B5563',
