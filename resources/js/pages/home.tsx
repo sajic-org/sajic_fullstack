7099f43513c0cf5f98c77cdbbc08978fda4c45e6
amaralTheSage <amaraldesouza9@gmail.com>
2025-04-08 21:23:15 -0300

    Adds link buttons styling

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link } from '@inertiajs/react';
+import { MousePointerClick } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -20,15 +21,20 @@ export default function Home() {
                 <div className="mx-auto mt-36 px-4 md:max-w-7xl">
                     <div className="md:w-2/3 dark:text-white">
                         <h1 className="text-3xl font-semibold">Sajic 5ª Edição | outubro de 2025</h1>
-                        <p className="mt-5 text-justify text-xl">
+                        <p className="mt-5 text-justify text-[22px]">
                             A Semana Acadêmica do UniSenac é um evento que oferece uma série de atividades acadêmicas, culturais e profissionais,
                             voltadas para o desenvolvimento de competências e a troca de conhecimentos entre alunos, professores e o público em geral.
                             A programação inclui palestras, oficinas, debates e workshops sobre temas diversos, como tecnologia, inovação,
                             diversidade, empreendedorismo, e tendências de mercado.
                         </p>
 
-                        <Link prefetch="mount" href="/palestras">
-                            <Button className="mt-8 px-6 py-3">Veja nossas palestras</Button>
+                        <Link
+                            prefetch="mount"
+                            href="/palestras"
+                            className="bg-primary-blue/90 hover:bg-primary-blue mt-8 flex h-fit w-fit items-center gap-3 rounded-lg px-9 py-3.5 text-xl font-medium text-white shadow-lg drop-shadow-md"
+                        >
+                            Veja nossas palestras
+                            <MousePointerClick size={23} className="mt-px" />
                         </Link>
                     </div>
                 </div>
