7bb6c9543b2d02de95870095cca904314d8c9ac0
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-04-22 16:43:31 -0300

    feature: adding breadcurmbs to checkIn page

--- resources/js/pages/check-in.tsx
+++ resources/js/pages/check-in.tsx
@@ -1,5 +1,23 @@
+import AppLayout from "@/layouts/app-layout";
+import {type BreadcrumbItem} from "@/types"
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Admin',
+        href: '/',
+    },
+    {
+        title: "Check in",
+        href: "#"
+    }
+];
+
 function CheckIn() {
-    return <div></div>;
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <div>cuuuuuuuuuuuu</div>
+        </AppLayout>
+    );
 }
 
 export default CheckIn;
