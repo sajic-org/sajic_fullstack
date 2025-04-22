f1f124c4d1a388d1cffd65247553e0260034bea1
Lorenzo Q Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2025-04-22 19:30:33 -0300

    Fix my-lectures

--- resources/js/components/my-lectures-list.tsx
+++ resources/js/components/my-lectures-list.tsx
@@ -3,7 +3,7 @@ import MyLecturesListItem from './my-lectures-list-item';
 function MyLecturesList({ lectures }: { lectures: Lecture[] }) {
     return (
         <div className="border-border mx-auto h-[28rem] w-full space-y-2 overflow-y-scroll rounded-xl border-2 p-2 pr-6 md:w-4/5 lg:w-full">
-            {lectures?.map((item, i) => <MyLecturesListItem key={i} lectures={item} />)}
+            {lectures?.map((item, i) => <MyLecturesListItem key={i} lecture={item} />)}
         </div>
     );
 }
