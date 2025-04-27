19b712f9d5236daa58adcec68799581d07f78c4c
amaralTheSage <amaraldesouza9@gmail.com>
2025-04-27 13:58:51 -0300

    fix?

--- resources/js/components/date-picker.tsx
+++ resources/js/components/date-picker.tsx
@@ -1,29 +1,32 @@
 'use client';
 
 import { format } from 'date-fns';
+
 import { Calendar as CalendarIcon } from 'lucide-react';
+
 import * as React from 'react';
 
 import { Button } from '@/components/ui/button';
+
 import { Calendar } from '@/components/ui/calendar';
+
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { cn, now } from '@/lib/utils';
+
+import { cn } from '@/lib/utils';
 
 export function DatePicker({ onSetData }) {
-    const [date, setDate] = React.useState(now());
-
-    React.useEffect(() => {
-        onSetData('date', format(date, 'dd/MM'));
-    }, [date]);
+    const [date, setDate] = React.useState<Date>();
 
     return (
         <Popover>
             <PopoverTrigger asChild>
                 <Button variant={'outline'} className={cn('mt-1 w-full justify-start text-left font-normal', !date && 'text-muted-foreground')}>
                     <CalendarIcon className="mr-2 h-4 w-4" />
+
                     {date ? format(date, 'dd/MM') : <span>Selecionar a data</span>}
                 </Button>
             </PopoverTrigger>
+
             <PopoverContent className="w-auto p-0">
                 <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
             </PopoverContent>
