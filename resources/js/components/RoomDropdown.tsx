3ee96ff05a21436f281bae33afdfabd3aa2f7f3f
amaralTheSage <amaraldesouza9@gmail.com>
2025-05-13 14:14:37 -0300

    Attempt at actual functioning unavailable room warning. Issue: only already selected room actively showing warning upon hover.

--- resources/js/components/RoomDropdown.tsx
+++ resources/js/components/RoomDropdown.tsx
@@ -2,7 +2,7 @@
 
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger } from '@/components/ui/select';
 import { Info } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { createPortal } from 'react-dom';
 import UnavailableRoomInfo from './unavailable-room-info';
 
@@ -12,12 +12,9 @@ export interface Room {
 }
 
 export function RoomDropdown({ children, rooms, onSetData }: { children: React.ReactNode; rooms: Room[]; onSetData: any }) {
-    const [showInfo, setShowInfo] = useState(false);
-    const portalRoot = document.getElementById('portal-root');
+    const [showInfo, setShowInfo] = useState<string>();
+    const portalRoot = document.body;
 
-    useEffect(() => {
-        console.log(showInfo);
-    }, [showInfo]);
     return (
         <Select
             onValueChange={(value) => {
@@ -32,22 +29,25 @@ export function RoomDropdown({ children, rooms, onSetData }: { children: React.R
                         return (
                             <div>
                                 <SelectItem value={room.number} key={room.number}>
-                                    <div className="flex w-full items-center justify-between">
-                                        <div className="mr-2 flex items-center gap-1">
+                                    <div className="pointer-events-none flex w-full items-center justify-between">
+                                        <div className="pointer-events-auto mr-2 flex items-center gap-1">
                                             {room.number}
                                             <p className="text-xs font-medium">cap.: {room.capacity}</p>
                                         </div>
-                                        <Info
-                                            strokeWidth={2.5}
-                                            size={4}
-                                            onClick={(e) => {
-                                                e.stopPropagation();
-                                                setShowInfo(true);
-                                            }}
-                                        />
+                                        <div
+                                            onMouseEnter={() => setShowInfo(room.number)}
+                                            onMouseLeave={() => setShowInfo('')}
+                                            className="pointer-events-auto"
+                                        >
+                                            <Info strokeWidth={2.5} size={20} />
+                                        </div>
                                     </div>
                                 </SelectItem>
-                                {portalRoot && createPortal(<UnavailableRoomInfo className={showInfo ? 'block' : 'hidden'} />, portalRoot)}
+                                {portalRoot &&
+                                    createPortal(
+                                        <UnavailableRoomInfo room={room} className={showInfo == room.number ? 'block' : 'hidden'} />,
+                                        portalRoot,
+                                    )}
                             </div>
                         );
                     })}
--- resources/js/components/unavailable-room-info.tsx
+++ resources/js/components/unavailable-room-info.tsx
@@ -1,9 +1,10 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
+import { Room } from './RoomDropdown';
 
-export default function UnavailableRoomInfo({ className }: { className?: string }) {
+export default function UnavailableRoomInfo({ className, room }: { className?: string; room: Room }) {
     return (
-        <div className={cn('absolute top-12 left-48 z-50 rounded-md bg-gray-900 p-4 font-medium text-white', className)}>
-            <p>A sala 201 está sendo utilizada nesse horario por:</p>
+        <div className={cn('absolute right-5 bottom-5 z-50 min-w-xs rounded-md bg-black/90 p-4 font-medium text-white', className)}>
+            <p>A sala {room.number} está sendo utilizada nesse horario por:</p>
             <ul className="mt-1 space-y-2">
                 <li>
                     - Rosquinha Caseira, <br /> das 16:15 às 17:00
