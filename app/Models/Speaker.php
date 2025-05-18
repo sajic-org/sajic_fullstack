c74a4a4affb61eb0ad14dc172f2d2843ffa3ccd3
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-05-18 16:00:47 -0300

    feat: Adding IDE helper

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @mixin IdeHelperSpeaker
+ */
 class Speaker extends Model
 {
     use HasFactory;
