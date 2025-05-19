ed8f6e1bc9fd7a256ca1a190c49fb16266de8f31
amaralTheSage <amaraldesouza9@gmail.com>
2025-05-19 12:12:10 -0300

    adjustment to seeder so that lectures created by it use minutes skipping 15 minutes, so it doesnt break the time selector dropdown on the editing page

--- database/factories/LectureFactory.php
+++ database/factories/LectureFactory.php
@@ -20,8 +20,8 @@ class LectureFactory extends Factory
             'title' => fake()->text(75),
             'type' => fake()->randomElement(['Gestão e Mercado', 'Tecnologia']),
             'date' => fake()->randomElement(['15/10', '16/10', '17/10', '18,10']),
-            'starts' => fake()->time('H:i'),
-            'ends' => fake()->time('H:i'),
+            'starts' => fake()->randomElement(['11:00', '11:30', '12:15', '12:45', '13:15', '13:45']),
+            'ends' => fake()->randomElement(['14:00', '14:30', '15:15', '15:45', '16:15', '16:45']),
             'speaker_id' => fake()->randomElement([1, 2, 3, 4, 5]),
             'room_number' => '404',
         ];
