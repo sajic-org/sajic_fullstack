2afed74a14c8e80f09ea7b5bb75c7c414c14594e
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-05-28 04:34:58 -0300

    feat: partial filter implementation

--- resources/js/components/presence-data-table.tsx
+++ resources/js/components/presence-data-table.tsx
@@ -1,66 +1,70 @@
-import { LecturePresence } from "@/types/models";
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
+import { LecturePresence } from '@/types/models';
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable, ColumnFiltersState } from '@tanstack/react-table';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './ui/table';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 interface DataTableProps {
-    columns: ColumnDef<LecturePresence, any>[]
-    data: LecturePresence[]
+    columns: ColumnDef<LecturePresence, any>[];
+    data: LecturePresence[];
+    filter: ColumnFiltersState;
+    setFilter: Dispatch<SetStateAction<ColumnFiltersState>>
 }
 
-function PresenceDataTable({ columns, data }: DataTableProps) {
+function PresenceDataTable({ columns, data, filter, setFilter }: DataTableProps) {
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
 
     const table = useReactTable({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
-    })
+    });
+
+    const Thead = (
+        <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                        return (
+                            <TableHead key={header.id}>
+                                {flexRender(header.column.columnDef.header, header.getContext())}
+                            </TableHead>
+                        );
+                    })}
+                </TableRow>
+            ))}
+        </TableHeader>
+    );
+
+    const Tbody = (
+        <TableBody>
+            {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                    <TableRow key={row.id}>
+                        {row.getVisibleCells().map((cell) => (
+                            <TableCell key={cell.id}>
+                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                ))
+            ) : (
+                <TableRow>
+                    <TableCell colSpan={columns.length} className="h-24 text-center">
+                        Aluno não encontrado.
+                    </TableCell>
+                </TableRow>
+            )}
+        </TableBody>
+    );
 
     return (
         <div className="rounded-md border">
             <Table>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                    </TableHead>
-                                )
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow
-                                key={row.id}
-                                data-state={row.getIsSelected() && "selected"}
-                            >
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                No results.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
+                {Thead}
+                {Tbody}
             </Table>
         </div>
-    )
+    );
 }
 
-export default PresenceDataTable
+export default PresenceDataTable;
--- resources/js/pages/attendance_list.tsx
+++ resources/js/pages/attendance_list.tsx
@@ -5,6 +5,8 @@ import { presenceColumns } from '@/lib/presence-columns';
 import { BreadcrumbItem, SharedData } from '@/types';
 import { LecturePresence } from '@/types/models';
 import { Head, usePage } from '@inertiajs/react';
+import { ColumnFiltersState } from '@tanstack/react-table';
+import { useState } from 'react';
 
 interface AttendanceListPageProps {
     attendees: LecturePresence[]
@@ -25,7 +27,7 @@ export default function AttendanceList({ attendees }: AttendanceListPageProps) {
         },
     ];
 
-
+    const [columnFilter, setColumnFilter] = useState<ColumnFiltersState>([]);
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
@@ -33,13 +35,17 @@ export default function AttendanceList({ attendees }: AttendanceListPageProps) {
 
             <header className='pt-10 px-10'>
                 <h1 className='text-2xl font-bold pb-5'>Listagem de Presenças</h1>
-                <div className='flex flex-wrap justify-between'>
+                <div className='flex md:flex-nowrap flex-wrap md:justify-between gap-5'>
                     <p>Verifique a presença dos alunos da UniSenac</p>
-                    <Input className='w-96' placeholder='Filtrar por nome...'/>
+                    <Input className='md:w-96' placeholder='Filtrar por nome...'/>
                 </div>
             </header>
             <main className='pt-5 px-10'>
-                <PresenceDataTable data={attendees} columns={presenceColumns} />
+                <PresenceDataTable
+                    data={attendees}
+                    columns={presenceColumns}
+                    filter={columnFilter}
+                    setFilter={setColumnFilter} />
             </main>
         </AppLayout>
     );
