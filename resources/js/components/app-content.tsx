e074673f5d0b4303d54406efe9d8769886e8931d
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-06-03 23:11:40 -0300

    feat: partial SSR fix

--- resources/js/components/app-content.tsx
+++ resources/js/components/app-content.tsx
@@ -1,17 +1,17 @@
 import { SidebarInset } from '@/components/ui/sidebar';
 import * as React from 'react';
 import { Toaster } from 'sonner';
+import { Config } from 'vendor/tightenco/ziggy/src/js';
 
-interface AppContentProps extends React.ComponentProps<'main'> {
+interface AppContentProps extends React.ComponentProps<'main'>, Config{
     variant?: 'header' | 'sidebar';
 }
 
-export function AppContent({ variant = 'header', children, ...props }: AppContentProps) {
+export function AppContent({ variant = 'header', location, children, ...props }: AppContentProps) {
     if (variant === 'sidebar') {
         return <SidebarInset {...props}>{children}</SidebarInset>;
     }
-    const currentPath = window.location.pathname;
-
+    const currentPath = location?.pathname;
     return (
         <>
             <main className={`${currentPath !== '/' && 'max-w-7xl'} mx-auto flex h-full w-full flex-1 flex-col rounded-xl`} {...props}>
