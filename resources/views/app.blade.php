22dbddb73ddab0e9d9c109e5b0b5a11ff00cf600
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-06-04 04:48:29 -0300

    fix: :(

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,14 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-    @routes
 
-    <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
-    <!-- <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..600;1,400..700&display=swap" rel="stylesheet">
 
+    @routes
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/ico">
 
     @viteReactRefresh
