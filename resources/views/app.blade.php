3428c688eb0f2e0128d8bc96d7bf37a557adb652
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-06-04 04:43:27 -0300

    fix: please

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    @routes
 
     <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
     <!-- <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> -->
@@ -15,7 +16,6 @@
 
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/ico">
 
-    @routes
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
     @inertiaHead
