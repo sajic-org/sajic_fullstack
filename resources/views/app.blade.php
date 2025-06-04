b1b71421c221eecfb015f1ec086853685817711d
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-06-04 05:24:19 -0300

    ultima tentativa

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
-    @inertiaHead
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        @inertiaHead
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..600;1,400..700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..600;1,400..700&display=swap" rel="stylesheet">
 
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/ico">
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/ico">
 
-</head>
+    </head>
 
-<body class="font-sans antialiased">
-    @viteReactRefresh
-    @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-    @routes
-    @inertia
+    <body class="font-sans antialiased">
+        @inertia
+        @viteReactRefresh
+        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @routes
 </body>
 
 </html>
