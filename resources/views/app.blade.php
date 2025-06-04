22937c8c2265651720ab9f93d21774486e29726b
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-06-04 05:09:28 -0300

    TEM QUE VIR ÉPICA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    @inertiaHead
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,8 +16,6 @@
 
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-    @inertiaHead
-    <script> </script>
     @routes
 </head>
 
