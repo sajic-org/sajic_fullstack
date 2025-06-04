39d6ea1b01980f12482087dd92aad714eb0ae67d
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-06-04 04:16:32 -0300

    Revert "fix: REMOVING USELESS CODE"
    This reverts commit 83c6d5414b6da49dcf840bb0b04537a0cee7796a.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,12 +7,15 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
+    <!-- <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..600;1,400..700&display=swap" rel="stylesheet">
 
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/ico">
 
+    @routes
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
     @inertiaHead
