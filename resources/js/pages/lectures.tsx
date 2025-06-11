b3b7d8ee4a60013a2e82ee9c793afbfc61d63eba
amaralTheSage <amaraldesouza9@gmail.com>
2025-06-11 11:51:25 -0300

    Adds fully functional search on lectures page

--- resources/js/pages/lectures.tsx
+++ resources/js/pages/lectures.tsx
@@ -6,6 +6,8 @@ import AppLayout from '@/layouts/app-layout';
 import { BreadcrumbItem } from '@/types';
 import { Lecture, User } from '@/types/models';
 import { Head } from '@inertiajs/react';
+import { Search } from 'lucide-react';
+import { useEffect, useState } from 'react';
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Home',
@@ -18,6 +20,16 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 function Lectures({ lectures, user }: { lectures: Lecture[]; user?: User }) {
+    const [query, setQuery] = useState('');
+    const [filteredLectures, setFilteredSpeakers] = useState<Lecture[]>(lectures);
+
+    useEffect(() => {
+        const results = lectures.filter(
+            (l: Lecture) => l.title.toLowerCase().includes(query.toLowerCase()) || l.speaker?.name.toLowerCase().includes(query.toLowerCase()),
+        );
+        setFilteredSpeakers(results);
+    }, [query, lectures]);
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head>
@@ -44,6 +56,19 @@ function Lectures({ lectures, user }: { lectures: Lecture[]; user?: User }) {
                 linkUrl=""
                 title="Palestras Esgotadas:"
             />
+            <div className="px-2">
+                <div className="mx-auto mt-2 flex w-full items-center gap-3 rounded-lg border border-gray-300 bg-white px-3 py-1.5 shadow-sm md:w-2/5">
+                    <Search color="gray" size={18} className="mb-0.5" />
+                    <input
+                        type="text"
+                        placeholder="Busque por palestras ou palestrantes"
+                        className="w-full outline-none"
+                        onChange={(e) => {
+                            setQuery(e.target.value);
+                        }}
+                    />
+                </div>
+            </div>
 
             <div className="mt-10 px-4 sm:px-6 md:max-w-7xl">
                 <h2 className="text-primary-blue text-xl font-semibold">Palestras</h2>
@@ -51,29 +76,49 @@ function Lectures({ lectures, user }: { lectures: Lecture[]; user?: User }) {
             </div>
 
             <section className="xs:px-4 mx-auto w-full space-y-4 px-2 sm:px-6 md:max-w-7xl">
-                <LecturesGridHeading
-                    title="Tecnologia"
-                    description="Explore o mercado atual da tecnologia"
-                    image="https://phoenixnap.com/glossary/wp-content/uploads/2022/07/what-is-a-data-center.jpg"
-                    className={'bg-primary-blue mt-8 aspect-[3/2] text-white md:max-w-lg'}
-                />
+                {query ? (
+                    <div className="ml-5">
+                        <h4 className="mt-6 mb-1 text-2xl font-semibold">Tecnologia</h4>
+                        <div className="h-0.5 w-4/5 bg-gray-300 sm:w-1/3" />
+                    </div>
+                ) : (
+                    <LecturesGridHeading
+                        title="Tecnologia"
+                        description="Explore o mercado atual da tecnologia"
+                        image="https://phoenixnap.com/glossary/wp-content/uploads/2022/07/what-is-a-data-center.jpg"
+                        className={'bg-primary-blue mt-8 aspect-[3/2] text-white md:max-w-lg'}
+                    />
+                )}
 
                 <LecturesGrid>
-                    {lectures
+                    {filteredLectures
                         ?.filter((lecture) => lecture.type === 'Tecnologia')
                         .map((item, i) => <LecturesGridItem key={i} lecture={item} user={user} className="md:col-span-1" />)}
+                    {filteredLectures?.filter((lecture) => lecture.type === 'Tecnologia').length === 0 && (
+                        <div className="col-span-1 text-center text-gray-500">Nenhuma palestra encontrada para o termo pesquisado.</div>
+                    )}
                 </LecturesGrid>
 
-                <LecturesGridHeading
-                    title="Gestão e Mercado"
-                    image="https://www.ziprecruiter.com/svc/fotomat/public-ziprecruiter/cms/929029342ArchitecturalEngineer.jpg=ws1280x960"
-                    description="Explore the birth of groundbreaking ideas and inventions."
-                    className={'bg-primary-blue mt-8 ml-auto aspect-[3/2] text-white md:max-w-lg'}
-                />
+                {query ? (
+                    <div className="mr-5 flex flex-col items-end text-right">
+                        <h4 className="mt-6 mb-1 text-2xl font-semibold">Gestão e Mercado</h4>
+                        <div className="h-0.5 w-4/5 bg-gray-300 sm:w-1/3" />
+                    </div>
+                ) : (
+                    <LecturesGridHeading
+                        title="Gestão e Mercado"
+                        image="https://www.ziprecruiter.com/svc/fotomat/public-ziprecruiter/cms/929029342ArchitecturalEngineer.jpg=ws1280x960"
+                        description="Explore the birth of groundbreaking ideas and inventions."
+                        className={'bg-primary-blue mt-8 ml-auto aspect-[3/2] text-white md:max-w-lg'}
+                    />
+                )}
                 <LecturesGrid>
-                    {lectures
+                    {filteredLectures
                         ?.filter((lecture) => lecture.type === 'Gestão e Mercado')
                         .map((item, i) => <LecturesGridItem key={i} lecture={item} user={user} className="md:col-span-1" />)}
+                    {filteredLectures?.filter((lecture) => lecture.type === 'Gestão e Mercado').length === 0 && (
+                        <div className="col-span-1 text-center text-gray-500">Nenhuma palestra encontrada para o termo pesquisado.</div>
+                    )}
                 </LecturesGrid>
             </section>
             <section className="flex flex-col gap-6 p-5">
