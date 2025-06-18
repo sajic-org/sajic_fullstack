0ba6c3513a2e3bad7379b2ccdc8d97c4b4fb660a
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-06-18 18:07:28 -0300

    fix: dialog be too big in mobile

--- resources/js/components/speaker-drawer.tsx
+++ resources/js/components/speaker-drawer.tsx
@@ -7,7 +7,7 @@ function SpeakerDialog({ children, speaker }: { children: ReactNode; speaker: Sp
     return (
         <Dialog>
             <DialogTrigger asChild>{children}</DialogTrigger>
-            <DialogContent className="sm:max-w-[425px] md:max-w-3xl">
+            <DialogContent className="max-w-[350px] max-h-[90vh] sm:max-w-[425px] md:max-w-3xl">
                 <DialogHeader className="p-4">
                     <DialogTitle className="text-primary-blue text-xl">{speaker?.name}</DialogTitle>
                     <div className="flex-col items-start gap-4 max-md:flex md:grid md:grid-cols-5">
