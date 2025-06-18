4f669e439b97a583185d493e0f952b374791122f
amaralTheSage <amaraldesouza9@gmail.com>
2025-06-18 18:30:37 -0300

    atualizacao

--- resources/js/components/speaker-drawer.tsx
+++ resources/js/components/speaker-drawer.tsx
@@ -7,7 +7,7 @@ function SpeakerDialog({ children, speaker }: { children: ReactNode; speaker: Sp
     return (
         <Dialog>
             <DialogTrigger asChild>{children}</DialogTrigger>
-            <DialogContent className="max-w-[350px] max-h-[90vh] overflow-y-scroll sm:max-w-[425px] md:max-w-3xl">
+            <DialogContent className="h-[92vh] max-w-[350px] overflow-y-scroll sm:max-w-[450px] md:max-w-3xl">
                 <DialogHeader className="p-4">
                     <DialogTitle className="text-primary-blue text-xl">{speaker?.name}</DialogTitle>
                     <div className="flex-col items-start gap-4 max-md:flex md:grid md:grid-cols-5">
@@ -16,9 +16,10 @@ function SpeakerDialog({ children, speaker }: { children: ReactNode; speaker: Sp
                             alt={`Foto de ${speaker?.name}`}
                             className="col-span-2 mx-auto aspect-square w-full rounded-xl object-cover object-center max-md:max-w-sm"
                         />
-                        <DialogDescription className="col-span-3 h-full max-w-full px-4">{speaker?.description}</DialogDescription>
+                        <DialogDescription className="col-span-3 h-full max-w-full text-left md:px-4">{speaker?.description}</DialogDescription>
                     </div>
                 </DialogHeader>
+
                 <ul className="h-60 overflow-y-scroll px-4 pb-4">
                     <h4 className="text-lg font-bold text-neutral-800">
                         Palestras de <span className="text-primary-blue">{speaker?.name}</span>
