a416f2006b980599ed3574a7c4a672a60cf0c616
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-06-18 22:21:58 -0300

    fix: putting nome completo on label just to be sure STUPID people don't miss it

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -58,7 +58,7 @@ export default function Register() {
             <form className="flex flex-col gap-6" onSubmit={submit}>
                 <div className="grid gap-6">
                     <div className="grid gap-2">
-                        <Label htmlFor="name">Nome</Label>
+                        <Label htmlFor="name">Nome Completo</Label>
                         <Input
                             id="name"
                             type="text"
