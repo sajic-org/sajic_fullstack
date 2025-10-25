9c25966d088939c47026da8422e4a7af6c3523c6
amaralTheSage <amaraldesouza9@gmail.com>
2025-10-24 22:41:08 -0300

    Updates building names to goncalves and felix in the seeder (to be more descriptive to attendees)

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -12,29 +12,29 @@ class RoomSeeder extends Seeder
      */
     public function run(): void
     {
-        Room::create(['number' => 'Biblioteca', 'capacity' => 24, 'building' => 'principal']);
-        Room::create(['number' => 'Idiomas', 'capacity' => 21, 'building' => 'principal']);
-        Room::create(['number' => 'LabTec', 'capacity' => 60, 'building' => 'principal']);
-        Room::create(['number' => '102', 'capacity' => 85, 'building' => 'principal']);
-        Room::create(['number' => '201', 'capacity' => 50, 'building' => 'principal']);
-        Room::create(['number' => '202', 'capacity' => 50, 'building' => 'principal']);
-        Room::create(['number' => '203', 'capacity' => 30, 'building' => 'principal']);
-        Room::create(['number' => '204', 'capacity' => 50, 'building' => 'principal']);
-        Room::create(['number' => '205', 'capacity' => 53, 'building' => 'principal']);
-        Room::create(['number' => '206', 'capacity' => 50, 'building' => 'principal']);
-        Room::create(['number' => '207', 'capacity' => 50, 'building' => 'principal']);
-        Room::create(['number' => '208', 'capacity' => 50, 'building' => 'principal']);
-        Room::create(['number' => '209', 'capacity' => 12, 'building' => 'principal']);
-        Room::create(['number' => '210', 'capacity' => 25, 'building' => 'principal']);
+        Room::create(['number' => 'Biblioteca', 'capacity' => 24, 'building' => 'goncalves']);
+        Room::create(['number' => 'Idiomas', 'capacity' => 21, 'building' => 'goncalves']);
+        Room::create(['number' => 'LabTec', 'capacity' => 60, 'building' => 'goncalves']);
+        Room::create(['number' => '102', 'capacity' => 85, 'building' => 'goncalves']);
+        Room::create(['number' => '201', 'capacity' => 50, 'building' => 'goncalves']);
+        Room::create(['number' => '202', 'capacity' => 50, 'building' => 'goncalves']);
+        Room::create(['number' => '203', 'capacity' => 30, 'building' => 'goncalves']);
+        Room::create(['number' => '204', 'capacity' => 50, 'building' => 'goncalves']);
+        Room::create(['number' => '205', 'capacity' => 53, 'building' => 'goncalves']);
+        Room::create(['number' => '206', 'capacity' => 50, 'building' => 'goncalves']);
+        Room::create(['number' => '207', 'capacity' => 50, 'building' => 'goncalves']);
+        Room::create(['number' => '208', 'capacity' => 50, 'building' => 'goncalves']);
+        Room::create(['number' => '209', 'capacity' => 12, 'building' => 'goncalves']);
+        Room::create(['number' => '210', 'capacity' => 25, 'building' => 'goncalves']);
 
         // -----
-        Room::create(['number' => '101-T', 'capacity' => 20, 'building' => 'tech']);
-        Room::create(['number' => '102-T', 'capacity' => 20, 'building' => 'tech']);
-        Room::create(['number' => '103-T', 'capacity' => 23, 'building' => 'tech']);
-        Room::create(['number' => '104-T', 'capacity' => 5, 'building' => 'tech']);
-        Room::create(['number' => '105-T', 'capacity' => 25, 'building' => 'tech']);
-        Room::create(['number' => '201-T', 'capacity' => 35, 'building' => 'tech']);
-        Room::create(['number' => '204-T', 'capacity' => 60, 'building' => 'tech']);
-        Room::create(['number' => 'MultiUso 201', 'capacity' => 50, 'building' => 'tech']);
+        Room::create(['number' => '101-T', 'capacity' => 20, 'building' => 'felix']);
+        Room::create(['number' => '102-T', 'capacity' => 20, 'building' => 'felix']);
+        Room::create(['number' => '103-T', 'capacity' => 23, 'building' => 'felix']);
+        Room::create(['number' => '104-T', 'capacity' => 5, 'building' => 'felix']);
+        Room::create(['number' => '105-T', 'capacity' => 25, 'building' => 'felix']);
+        Room::create(['number' => '201-T', 'capacity' => 35, 'building' => 'felix']);
+        Room::create(['number' => '204-T', 'capacity' => 60, 'building' => 'felix']);
+        Room::create(['number' => 'MultiUso 201-T', 'capacity' => 50, 'building' => 'felix']);
     }
 }
