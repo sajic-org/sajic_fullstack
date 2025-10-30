cb201298879052bf4cef9ded1c7fad25dae7bfab
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-10-30 00:36:27 -0300

    fix: ssr errors even when WE DO NOT USE IT ANYMORE

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,7 +2,7 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { hydrateRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 // import { initializeTheme } from './hooks/use-appearance';
 
 createInertiaApp({
@@ -13,7 +13,8 @@ createInertiaApp({
             import.meta.glob('./pages/**/*.tsx'),
         ),
     setup({ el, App, props }) {
-        hydrateRoot(el, <App {...props} />);
+        const root = createRoot(el);
+        root.render(<App {...props} />);
     },
     progress: {
         color: '#4B5563',
