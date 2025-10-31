72542b1a649cc5d133fc3d1462184cca42b5d03d
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-10-30 23:15:54 -0300

    feat: adjust email logo to be better in dark and white mode

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 @props(['url'])
 <tr>
     <td class="header">
-        <a href="https://sathon.com.br" style="display: inline-block; width: 180px; filter: invert(80%);">
-            <img src="https://sathon.com.br/assets/logo_nova-branca.png" alt="SATHON 2025">
+        <a href="https://sathon.com.br" style="display: inline-block; width: 180px;">
+            <img src="https://sathon.com.br/assets/sathon_fresco.png" alt="SATHON 2025">
         </a>
     </td>
 </tr>
