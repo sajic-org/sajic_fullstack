594f3132823c7640f36ce72ea7d462eedbadd3f5
amaralTheSage <amaraldesouza9@gmail.com>
2025-11-05 12:34:20 -0300

    visual fix to buttons

--- resources/js/pages/check-in.tsx
+++ resources/js/pages/check-in.tsx
@@ -15,7 +15,6 @@ import { Lecture } from '@/types/models';
 
 import { FormDataConvertible } from '@inertiajs/core';
 
-import Spinner from '@/components/spinner';
 import checkInColumns from '@/lib/check-in-columns-factory';
 import { toast } from 'sonner';
 
@@ -139,26 +138,22 @@ function CheckIn({ lecture }: checkInPageProps) {
                                 form.patch(`/palestras/${lecture.id}/check-in`)
                             }
                         >
-                            {form.processing ? (
-                                <Spinner size={8} />
-                            ) : (
-                                <>
-                                    <svg
-                                        className="h-4 w-4"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
-                                        />
-                                    </svg>{' '}
-                                    Salvar
-                                </>
-                            )}
+                            <>
+                                <svg
+                                    className="h-4 w-4"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
+                                    />
+                                </svg>{' '}
+                                Salvar
+                            </>
                         </button>
 
                         <button
